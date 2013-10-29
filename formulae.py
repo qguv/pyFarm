7b7845a38d126a6510f2b22bbdad16b9a2b9c2a6
Quint Guvernator <quintus.public@gmail.com>
2013-10-29 15:53:03 -0400

    whoops, need that colon

--- formulae.py
+++ formulae.py
@@ -7,7 +7,7 @@ def distance(point1, point2):
             "points given to distance must be of the same dimensionality"
     return math.sqrt(sum([ (a1 - a2) ** 2 for a1, a2 in zip(point1, point2) ]))
 
-def angle(point1, point2)
+def angle(point1, point2):
     assert len(point1) == len(point2), \
             "points given to distance must be of the same dimensionality"
     pass #TODO
